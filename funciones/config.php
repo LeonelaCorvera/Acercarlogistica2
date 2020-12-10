98953dc44778f4a523ff08f8dbd17d1ff4295060
leonelacorvera90@gmail.com <leonelacorvera90@gmail.com>
2020-12-10 20:24:52 -0300

    commit 2024

--- funciones/config.php
+++ funciones/config.php
@@ -1,8 +1,16 @@
 <?php
+	/*
 	//database CONSTANTES
 	define('SQL_SERVER','localhost');
 	define('SQL_USER','root');
 	define('SQL_PASS','');
 	//define('SQL_DB','friendface2');
 	define('SQL_DB','acercarlogistca');
+*/
+
+	define('SQL_SERVER','remotemysql.com');
+	define('SQL_USER','AU1sul0gxE');
+	define('SQL_PASS','TDTLZgtXMg');
+	//define('SQL_DB','friendface2');
+	define('SQL_DB','AU1sul0gxE');
 ?>
